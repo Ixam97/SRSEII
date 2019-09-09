e53ee35673445b9497bc457a4ef2eae3b9fdcf34
Ixam97 <maxigoldschmidt@gmail.com>
2019-09-09 23:25:28 +0200

    implementing attempts and timeouts

--- maecan-updater/maecanUpdater.c
+++ maecan-updater/maecanUpdater.c
@@ -45,7 +45,7 @@ fd_set readfds;
 
 uint8_t page_buffer[PAGE_SIZE];
 uint8_t page_buffer_index;
-uint8_t page_count;
+uint16_t page_count;
 uint8_t page_index;
 
 uint8_t remote_type;
@@ -53,6 +53,9 @@ uint8_t remote_pagesize;
 uint8_t remote_pagecount;
 
 uint8_t updating;
+uint8_t listening;
+uint8_t trys;
+uint8_t retry;
 
 uint8_t complete_buffer[0xffff];
 uint16_t complete_buffer_index;
@@ -184,99 +187,159 @@ void main(int argc, char *argv[]) {
 
     printf("Looking for Device with UID 0x%8x...\n", can_uid);
 
-    while (1) {
+    listening = 1;
+
+    
+
+    while (listening) {
+        uint8_t received_frame = 0;
+
         FD_ZERO(&readfds);
         FD_SET(socketcan, &readfds);
-
         /* Reading can frame: */
-        if (FD_ISSET(socketcan, &readfds)) {
-            if (read(socketcan, &frame, sizeof(struct can_frame))) {
+        struct timeval timeout = {1, 0};
 
-                uint8_t cmd = (uint8_t) (frame.can_id >> 17);
-                uint8_t resp = (uint8_t) ((frame.can_id >> 16) & 0x01);
-                /* uint16_t hash = (uint16_t) (frame.can_id); */
-                uint8_t dlc = frame.can_dlc;
-                uint8_t data[8];
-                for (uint8_t i = 0; i < dlc; i++) {
-                    data[i] = frame.data[i];
-                }
-                for (uint8_t i = dlc; i < 8; i++) {
-                    data[i] = 0;
-                }
+        if(select((socketcan + 1), &readfds, NULL, NULL, &timeout) >= 0) {
+            if (FD_ISSET(socketcan, &readfds)) {
+                if (read(socketcan, &frame, sizeof(struct can_frame))) {
 
-                /* check for relevant commands from CAN: */
-                if (cmd == 0x40 && (dlc == 6 || dlc == 7)) {
-                    /* check for UID */
-                    if ((data[0] == (uint8_t)(can_uid >> 24)) && (data[1] == (uint8_t)(can_uid >> 16)) && (data[2] == (uint8_t)(can_uid >> 8)) && (data[3] == (uint8_t)can_uid)) {
-                        switch (data[4]) {
-                            case 0x01 : {
-                                // Type:
-                                remote_type = data[5];
-                                break;
-                            }
-                            case 0x02 : {
-                                // Pagesize:
-                                remote_pagesize = data[5];
-                                break;
-                            }
-                            case 0x03 : {
-                                // Pagecount:
-                                remote_pagecount = data[5];
-                                break;
-                            }
-                            case 0x05 : {
-                                if (dlc == 7 && data[5] == (page_index - 1)) {
-                                    if (data[6] == 0) {
-                                        // Transmisson error, new attempt:
-                                        sendPage(page_index - 1);
-                                    } else if (page_index < page_count) {
-                                        if (data[6] == 1) {
-                                            // Transfer successfull, next page:
-                                            sendPage(page_index);
-                                            page_index++;
-                                        }
-                                    } else {
-                                        // Update complete
-                                        out_data[4] = 0x07;
-                                        sendCanFrame(0x00800300, 6, out_data);
-                                        printf("Update completed.\n");
-                                        exit(EXIT_SUCCESS);
-                                    }
-                                    
-                                }
-                            }
-                            default : {
-                                break;
-                            }
-                        }
+                    received_frame = 1;
+
+                    uint8_t cmd = (uint8_t) (frame.can_id >> 17);
+                    uint8_t resp = (uint8_t) ((frame.can_id >> 16) & 0x01);
+                    /* uint16_t hash = (uint16_t) (frame.can_id); */
+                    uint8_t dlc = frame.can_dlc;
+                    uint8_t data[8];
+                    for (uint8_t i = 0; i < dlc; i++) {
+                        data[i] = frame.data[i];
+                    }
+                    for (uint8_t i = dlc; i < 8; i++) {
+                        data[i] = 0;
                     }
 
-                    if (remote_type != 0 && remote_pagecount != 0 && remote_pagesize != 0 && updating == 0){
-                        // Check for compatibility:
-                        updating = 1;
-                        out_data[4] = 0x01;
-                        out_data[5] = remote_type;
-                        page_count = (complete_buffer_index / remote_pagesize) + 1;
-                        printf("Byte Count: %d, Page count: %d\n", complete_buffer_index, page_count);
-                        if (page_count > remote_pagecount) {
-                            // Update too large:
-                            printf("New Update too large, aborting.\n");
-                            out_data[6] = 0;
-                            sendCanFrame(0x00800300, 7, out_data);
-                            exit(EXIT_SUCCESS);
-
-                        } else {
-                            printf("Beginning update...\n");
-                            out_data[6] = 1;
-                            sendCanFrame(0x00810300, 7, out_data);
-
-                            sendPage(page_index);
-                            page_index++;
+                    /* check for relevant commands from CAN: */
+                    if (cmd == 0x40 && (dlc == 6 || dlc == 7)) {
+                        /* check for UID */
+                        if ((data[0] == (uint8_t)(can_uid >> 24)) && (data[1] == (uint8_t)(can_uid >> 16)) && (data[2] == (uint8_t)(can_uid >> 8)) && (data[3] == (uint8_t)can_uid)) {
+                            
+                            switch (data[4]) {
+                                case 0x01 : {
+                                    // Type:
+                                    remote_type = data[5];
+                                    break;
+                                }
+                                case 0x02 : {
+                                    // Pagesize:
+                                    remote_pagesize = data[5];
+                                    break;
+                                }
+                                case 0x03 : {
+                                    // Pagecount:
+                                    remote_pagecount = data[5];
+                                    break;
+                                }
+                                case 0x05 : {
+                                    if (dlc == 7 && data[5] == (page_index - 1)) {
+                                        if (data[6] == 0) {
+                                            // Transmisson error, new attempt:
+                                            printf("Page error, new attempt...\n");
+                                            sendPage(page_index - 1);
+                                        } else if (page_index < page_count) {
+                                            if (data[6] == 1) {
+                                                // Transfer successfull, next page:
+                                                sendPage(page_index);
+                                                page_index++;
+                                            }
+                                        } else {
+                                            // Update complete
+                                            out_data[4] = 0x07;
+                                            sendCanFrame(0x00800300, 6, out_data);
+                                            printf("Update completed.\n");
+                                            close(socketcan);
+                                            exit(EXIT_SUCCESS);
+                                        }
+                                        
+                                    }
+                                }
+                                default : {
+                                    //break;
+                                }
+                            }
                         }
 
+                        if (remote_type != 0 && remote_pagecount != 0 && remote_pagesize != 0 && updating == 0){
+                            // Check for compatibility:
+                            updating = 1;
+                            out_data[4] = 0x01;
+                            out_data[5] = remote_type;
+                            page_count = (complete_buffer_index / remote_pagesize) + 1;
+                            if (retry == 0) {
+                                printf("Device found! \nByte Count: %d, Page count: %d\n", complete_buffer_index, page_count);
+                            }
+                            if (page_count > remote_pagecount) {
+                                // Update too large:
+                                printf("New Update too large, aborting.\n");
+                                out_data[6] = 0;
+                                sendCanFrame(0x00800300, 7, out_data);
+                                exit(EXIT_FAILURE);
+
+                            } else {
+                                
+                                if (retry == 0) {
+                                    usleep(1000000);
+                                    printf("Beginning update...\n");
+                                    page_index = 1;
+
+                                } else {
+
+                                    out_data[6] = 1;
+                                    sendCanFrame(0x00810300, 7, out_data);
+                                    printf("Blub\n");
+                                    sendPage(page_index);
+                                    page_index++;
+                                }
+                            }
+
+                        }
                     }
                 }
             }
+        }
+
+
+        
+        if (updating == 0 && trys < 10 && received_frame == 0) {
+            trys++;
+            printf("Try %d ... \n", trys);
+
+            out_data[0] = (can_uid >> 24) & 0xff;
+            out_data[1] = (can_uid >> 16) & 0xff;
+            out_data[2] = (can_uid >> 8) & 0xff;
+            out_data[3] = (can_uid) & 0xff;
+
+            sendCanFrame(0x00800300, 4, out_data);
+
+        } else if (trys >= 10) {
+            printf("Device unreachable!\n");
+            listening = 0;
+            
+            close(socketcan);
+            //break;
+        } else if (updating == 1 && page_index == 1 && received_frame == 0) {
+            if (retry >= 1) {
+                printf("transmission error! \n");
+                trys++;
+            }
+            page_index = 0;
+            updating = 0;
+            retry = 1;
+
+            out_data[0] = (can_uid >> 24) & 0xff;
+            out_data[1] = (can_uid >> 16) & 0xff;
+            out_data[2] = (can_uid >> 8) & 0xff;
+            out_data[3] = (can_uid) & 0xff;
+
+            sendCanFrame(0x00800300, 4, out_data);
         }
     }
     close(socketcan);
